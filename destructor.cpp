e55bff20f069856da65f1848ffa396ad77ac7d0d
Priyal Gupta <167235958+itzzpriyal@users.noreply.github.com>
2024-07-02 20:37:34 +0530

    Day 4 : Program 1

---
+++ destructor.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int count=0;//GLOBAL VARIABLE
+/*never takes any argument nor does it return any value
+destructors are used to destroy the memory which was being used by the object*/
+
+class num{
+    public:
+    num(){
+    count++;
+    cout<<"Constructor is being called for object no."<<count<<endl;
+    }
+    ~num(){
+        cout<<"destructor is being called for object no."<<count<<endl;
+     count--;
+    }
+};
+int main(){
+    cout<<"Entering main block"<<endl;
+    cout<<"creating first object"<<endl;
+    num object1;
+    {//creating another block such that the scope of objects remain created here ,remain here only n once it reaches the end of block
+    //it gets destroyed.
+    cout<<"Enterning a new block"<<endl;
+    cout<<"creating two more new objects"<<endl;
+    num object2,object3;
+    cout<<"end of block 2"<<endl;
+     }
+     cout<<"end of main block"<<endl;
+return 0;
+}
