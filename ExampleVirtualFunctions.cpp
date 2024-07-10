b70232b58261295f8e511149f9231b60ed853038
Priyal Gupta <167235958+itzzpriyal@users.noreply.github.com>
2024-07-10 20:10:16 +0530

    Day 12 : Program 2

---
+++ ExampleVirtualFunctions.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+class tmi
+{
+protected:
+    string title;
+    float rating;
+
+public:
+    tmi(string s, float r)
+    {
+        title = s;
+        rating = r;
+    }
+    virtual void display() {}//as we had made display function virtual therefore the display function of the class to which object belongs will work
+};
+class tmibones : public tmi
+{
+    int bookno;
+
+public:
+    tmibones(string s, float r, int b_no) : tmi(s, r)
+    { // syntax we learnt earlier
+        bookno = b_no;
+    }
+    void display()
+    {
+        cout << "NAME OF BOOK : " << title << endl;
+        cout << "BOOK RATING : " << rating << endl;
+        cout << "BOOK NO. : " << bookno << endl;
+    }
+};
+class tmiglass : public tmi
+{
+    int booklength;
+
+public:
+    tmiglass(string s, float r, int b_lenth) : tmi(s, r)
+    { // syntax we learnt earlier (i.e)constructor for derived class
+        booklength = b_lenth;
+    }
+    void display()
+    {
+        cout << "@NAME OF BOOK : " << title << endl;
+        cout << "@BOOK RATING : " << rating << endl;
+        cout << "@BOOK NO. : " << booklength << endl;
+    }
+};
+
+int main()
+{
+    string titlebook;
+    float ratingbook;
+    int bookno, booklength;
+    // for derived class city of bones
+    titlebook = "city of bones";
+    ratingbook = 5;
+    bookno = 1;
+    tmibones object_bones(titlebook, ratingbook, bookno);
+    // object_bones.display();--------->for directly running display function
+    // for derived class city of glass
+    titlebook = "city of glass";
+    ratingbook = 4.9;
+    bookno = 3;
+    tmiglass object_glass(titlebook, ratingbook, bookno);
+    // object_glass.display();;--------->for directly running display function
+    // ###################################
+    // METHOD 1:either make two separate pointers
+
+    tmi *ptr;
+    ptr = &object_bones;
+    ptr->display();
+    tmi *ptr2;
+    ptr2 = &object_glass;
+    ptr2->display();
+    cout<<"#######################################################################"<<endl;
+
+    // ####################################
+    // METHOD 2:make array of pointers
+    tmi *PTR[2];
+    PTR[0] = &object_bones;
+    PTR[1] = &object_glass;
+
+    PTR[0]->display();
+    cout << endl;
+    PTR[1]->display();
+
+    return 0;
+}
